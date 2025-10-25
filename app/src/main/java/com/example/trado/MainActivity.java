9cacc48692d18f489cf7f2d011cc2e9bf72681e9
akhi <akhileshk14354@gmail.com>
2025-10-25 22:21:51 +0530

    video-1

--- app/src/main/java/com/example/trado/MainActivity.java
+++ app/src/main/java/com/example/trado/MainActivity.java
@@ -1,24 +1,102 @@
 package com.example.trado;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.trado.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        showHomeFragment();
+
+        binding.bottomNv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int itemId = menuItem.getItemId();
+                if(itemId == R.id.menu_home)
+                {
+                    showHomeFragment();
+
+                    return true;
+                } else if (itemId == R.id.menu_chats)
+                {
+                    showChatsFragment();
+
+                    return true;
+                } else if (itemId == R.id.menu_my_ads)
+                {
+                    showMyAdsFragment();
+
+                    return true;
+                } else if (itemId == R.id.menu_account)
+                {
+                    showAccountFragment();
+
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void showHomeFragment() {
+        binding.toolbarTitleTv.setText("Home");
+
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "HomeFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void showChatsFragment() {
+        binding.toolbarTitleTv.setText("Chats");
+
+        ChatsFragment fragment = new ChatsFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "ChatsFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void showMyAdsFragment() {
+        binding.toolbarTitleTv.setText("My Ads");
+
+        MyAdsFragment fragment = new MyAdsFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "MyAdsFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void showAccountFragment() {
+        binding.toolbarTitleTv.setText("Account");
+
+        AccountFragment fragment = new AccountFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "AccountFragment");
+        fragmentTransaction.commit();
     }
 }
