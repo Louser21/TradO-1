fb666f4c81b249e961e63b0a275eb9d8ffbf0b9d
Nitin Bhandari <nitin211206@gmail.com>
2025-10-25 22:43:07 +0530

    check-2

--- app/src/main/java/com/example/trado/LoginEmailActivity.java
+++ app/src/main/java/com/example/trado/LoginEmailActivity.java
@@ -1,24 +1,113 @@
 package com.example.trado;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.trado.databinding.ActivityLoginEmailBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginEmailActivity extends AppCompatActivity {
+
+    private ProgressDialog progressDialog;
+    private static final String TAG = "LOGIN_TAG";
+    private FirebaseAuth firebaseAuth;
+    private ActivityLoginEmailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_email);
+        binding = ActivityLoginEmailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+
+
+        binding.toolbarBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private String email, password;
+
+    private void validateData(){
+        email = binding.emailEt.getText().toString().trim();
+        password = binding.passwordEt.getText().toString().trim();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.emailTil.setError("Invalid Email Format");
+            binding.emailEt.requestFocus();
+        }else if (!email.matches(".*@(nitw\\.ac\\.in|students\\.nitw\\.ac\\.in)$")) {
+            binding.emailTil.setError("Email must be a NITW domain");
+            binding.emailEt.requestFocus();
+        }
+        else if(password.isEmpty()){
+            binding.passwordEt.setError("Password cannot be empty");
+            binding.passwordEt.requestFocus();
+        } else {
+            loginUser();
+        }
+    }
+
+
+    private void loginUser(){
+        progressDialog.setMessage("Logging In...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Log.d(TAG, "onSuccess: Logged in");
+                        progressDialog.dismiss();
+                        startActivity(new Intent(LoginEmailActivity.this, MainActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener(){
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: " + e);
+                        Utils.toast(LoginEmailActivity.this, "Failed due to " + e.getMessage());
+                        progressDialog.dismiss();
+                    }
+                });
     }
 }
